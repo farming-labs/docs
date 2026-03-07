a84504fe4221930bedd3013eacc15cdc4fcadb56
Kinfe123 <kinfishtech@gmail.com>
2026-03-07 17:43:28 +0300

    chore: chitshit

--- website/app/showcase/page.tsx
+++ website/app/showcase/page.tsx
@@ -159,7 +159,9 @@ export default function ShowcasePage() {
               </p>
               <p className="text-[13px] text-neutral-500 dark:text-white/40 leading-relaxed">
                 <span className="lg:hidden">Submit yours below.</span>
-                <span className="hidden lg:inline">Submit yours to see your documentation site here.</span>
+                <span className="hidden lg:inline">
+                  Submit yours to see your documentation site here.
+                </span>
               </p>
             </div>
           </div>
