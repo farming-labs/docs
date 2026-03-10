e624e8a720a6a39c73b362cfbfde743af20d1851
Kinfe123 <kinfishtech@gmail.com>
2026-03-10 23:19:05 +0300

    chore: tests

--- examples/sveltekit/.svelte-kit/output/server/nodes/1.js
+++ examples/sveltekit/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.0sNVwfN6.js","_app/immutable/chunks/BVoj8vGL.js","_app/immutable/chunks/D-GgqtQO.js","_app/immutable/chunks/VDpXBhJE.js","_app/immutable/chunks/aCuBA-jg.js","_app/immutable/chunks/BnaCWC9X.js","_app/immutable/chunks/DzywazqE.js"];
+export const imports = ["_app/immutable/nodes/1.B70Nk9lF.js","_app/immutable/chunks/BVoj8vGL.js","_app/immutable/chunks/D-GgqtQO.js","_app/immutable/chunks/VDpXBhJE.js","_app/immutable/chunks/aCuBA-jg.js","_app/immutable/chunks/DMjtvcdx.js","_app/immutable/chunks/DzywazqE.js"];
 export const stylesheets = [];
 export const fonts = [];
