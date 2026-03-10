c70ffc7128a24c2b4b83add3534d2b1773a57d03
Kinfe123 <kinfishtech@gmail.com>
2026-03-10 17:50:30 +0300

    chore: chitshit

--- examples/nuxt/.output/public/_nuxt/yjIL6nQ_.js
+++ examples/nuxt/.output/public/_nuxt/yjIL6nQ_.js
@@ -1,1 +1,15 @@
-import{ac as a,ad as s,ae as r,j as u,af as o}from"./UEFHYZlh.js";function c(e){const t=e||s();return t?.ssrContext?.head||t?.runWithContext(()=>{if(r())return u(o)})}function d(e,t={}){const n=c(t.nuxt);if(n)return a(e,{head:n,...t})}export{d as u};
+import { ac as a, ad as s, ae as r, j as u, af as o } from "./UEFHYZlh.js";
+function c(e) {
+  const t = e || s();
+  return (
+    t?.ssrContext?.head ||
+    t?.runWithContext(() => {
+      if (r()) return u(o);
+    })
+  );
+}
+function d(e, t = {}) {
+  const n = c(t.nuxt);
+  if (n) return a(e, { head: n, ...t });
+}
+export { d as u };
