1707d847d14870a716dd5896435e5570855d3389
KinfeMichael Tariku <65047246+Kinfe123@users.noreply.github.com>
2026-03-19 00:00:46 +0300

    chore: changelogs v0.0.44 (#46)
    * chore: changelog v0.0.44
    
    * chore: fmt

--- website/docs.config.tsx
+++ website/docs.config.tsx
@@ -218,9 +218,9 @@ export default defineDocs({
               textDecorationColor:
                 "color-mix(in srgb, var(--color-fd-foreground) 30%, transparent)",
             }}
-            href="/changelog#v0.0.31"
+            href="/changelog#v0.0.44"
           >
-            v0.0.31
+            v0.0.44
           </a>
         </div>
         <span className="uppercase font-mono text-[10px] tracking-tight block">
