7c786740bec847ba54046502bf02b544fd46a73a
Kinfe123 <kinfishtech@gmail.com>
2026-05-01 18:25:56 +0300

    chore: clean up

--- .tmp-remote-openapi-smoke/.docs-cloud/site/app/api-reference/[[...slug]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import docsConfig from "@/docs.config";
-import { createNextApiReferencePage } from "@farming-labs/next/api-reference";
-
-const ApiReferencePage = createNextApiReferencePage(docsConfig);
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default ApiReferencePage;
--- .tmp-remote-openapi-smoke/.docs-cloud/site/app/api-reference/layout.tsx
+++
@@ -1,8 +0,0 @@
-import docsConfig from "@/docs.config";
-import { createNextApiReferenceLayout } from "@farming-labs/next/api-reference";
-
-const ApiReferenceLayout = createNextApiReferenceLayout(docsConfig);
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ApiReferenceLayout>{children}</ApiReferenceLayout>;
-}
