adc4ffc76783ac29e78bc25016c838841616d1e2
Kinfe123 <kinfishtech@gmail.com>
2026-06-13 03:58:05 +0300

    fix: align sveltekit build target

--- examples/sveltekit/vite.config.js
+++ examples/sveltekit/vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    target: "es2022",
+  },
   plugins: [sveltekit()],
 });
