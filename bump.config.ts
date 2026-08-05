72d77509df27aee225f44dfcdb7f29cb0df2b24b
KinfeMichael Tariku <65047246+Kinfe123@users.noreply.github.com>
2026-08-05 22:31:08 +0300

    fix: include Farm adapter in releases (#443)

--- bump.config.ts
+++ bump.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     "packages/svelte-theme/package.json",
     "packages/astro/package.json",
     "packages/astro-theme/package.json",
+    "packages/farmjs/package.json",
     "packages/nuxt/package.json",
     "packages/nuxt-theme/package.json",
   ],
